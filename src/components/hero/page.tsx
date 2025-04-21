4c6549ed18775a12a308818f579f884d89200463
abhay Name <abhay@email.com>
2025-04-21 14:05:16 +0530

    feat: add Hero component styles and page

---
+++ src/components/hero/page.tsx
@@ -0,0 +1,31 @@
+// src/pages/index.tsx
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/components/hero/hero.module.scss';
+import logo from '@/public/logo.png'; // Adjust the path as necessary
+
+const First: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.logo}>
+          <Image 
+            src={logo} 
+            alt="Digital First Media Logo" 
+            width={100} 
+            height={60} 
+          />
+        </div>
+        
+        <h1 className={styles.title}>
+          The First Media Company crafted For the<br />
+          Digital First generation
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default First;
